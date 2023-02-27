d4da96ff29276f3d67ef81c079246aa7bfd0809d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-02-27 16:55:01 +0200

    Nav css update
    update of nav buttons

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,22 +1,25 @@
 import './Navigation.css'
 import { NavLink } from 'react-router-dom'
 
-export default function Navigation () {
+export default function Navigation() {
     return (
         <nav>
             <ul>
-            <li>
-                <div className="dropdown">
-                <button className="dropbtn">Dropdown</button>
-                <div className="dropdown-content">
-                    <ul>
-                    <li><NavLink to = "/">Products</NavLink></li>
-                    <li><NavLink to = "/">Deals</NavLink></li>
-                    <li><NavLink to = "/">Recycle your furniture</NavLink></li>
-                    </ul>
-                </div>
-            </div>
-            </li>
+            <li className='nav-li'><NavLink to="/">Home</NavLink></li>
+                <li className ="dropLi">
+                    <div className="dropdown">
+                        <button className="dropbtn">Menu</button>
+                        <div className="dropdown-content">
+                            <ul>
+                                <li><NavLink to="/">Products</NavLink></li>
+                                <li><NavLink to="/">Deals</NavLink></li>
+                                <li><NavLink to="/">Recycle your furniture</NavLink></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li className='nav-li'><NavLink to="/">Login</NavLink></li>
+                <li className='nav-li'><NavLink to="/">Register</NavLink></li>
             </ul>
         </nav>
     )
