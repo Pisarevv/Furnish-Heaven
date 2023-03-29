abf349d7f88ce1a2a98837dd7008a22ca3412c48
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-29 09:31:29 +0300

    Update Observer.js

--- client/src/utils/Observer.js
+++ client/src/utils/Observer.js
@@ -1,10 +1,19 @@
 const Observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        }
-    });
+
+        entries.forEach((entry) => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('show');
+            }
+        });
+    
+   
 });
 
 
-export default Observer;
+const Observe = () => {
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach(el => Observer.observe(el));
+
+}
+
+export default Observe;
