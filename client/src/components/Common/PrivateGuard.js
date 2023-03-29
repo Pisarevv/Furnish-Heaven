8519628737af1159c2da80648e9ae344166c3386
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-29 09:55:58 +0300

    Added component documentation to PrivateGuard

--- client/src/components/Common/PrivateGuard.js
+++ client/src/components/Common/PrivateGuard.js
@@ -1,3 +1,19 @@
+/**
+ * PrivateGuard Component
+ * ---------------------
+ * This component usage is to stop unauthorized requests to access parts of the
+ * website that are for users with authorization.
+ * The component wraps around components that require authentication.
+ * Example of a path that require authorization is editing a listing that a user has created.
+ * Unauthorized request to access a protected path redirects to the login page.
+ * 
+ * Contexts:
+ * - useAuthContext
+ * The context provides the "isAuthenticated" constant that 
+ * validates if an user is authenticated or not.
+ * ---------------------- 
+ * 
+ **/
 import { Navigate, Outlet } from "react-router-dom";
 
 import { useAuthContext } from "../../contexts/AuthContext";
