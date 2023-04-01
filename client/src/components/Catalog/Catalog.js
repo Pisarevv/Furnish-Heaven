4985536f1b7ba2105d5a66bdd26ca8ffec044837
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-04-01 19:02:10 +0300

    Minor fixes

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -65,79 +65,85 @@ import TrendingProductCard from './TrendingProductCard';
 import './Catalog.css'
 import Observe from '../../utils/Observer';
 
+
+
 const Catalog = () => {
 
-    const {userLogout} = useAuthContext();
-    const {emptyCart} = useCartContext();
-    
-    const [trendingProducts,setTrendingProducts] = useState([]);
+    const { userLogout } = useAuthContext();
+    const { emptyCart } = useCartContext();
+
+
+    const [trendingProducts, setTrendingProducts] = useState([]);
+    const [isLoading,setIsLoading] = useState(true);
+
 
     useEffect(() => {
-      (async () => {
-        try{
-            window.scrollTo(0, 0);
-            Observe();
-            const result = await getTrendingProducts();
-            setTrendingProducts(trendingProducts => result);
+        (async () => {
+            try {
+                window.scrollTo(0, 0);
+                Observe();
+                const result = await getTrendingProducts();
+                setTrendingProducts(trendingProducts => result);
+                setIsLoading(false)
+            }
+            catch (error) {
+                if (error === "Invalid access token") {
+                    ErrorHandler(error)
+                    userLogout();
+                    emptyCart();
+                };
+            }
         }
-        catch (error) {
-            if(error === "Invalid access token"){
-                ErrorHandler(error)
-                userLogout();
-                emptyCart();
-            };
-      }}
-      )()
-    },[])
-    
+        )()
+    }, [])
+
 
     return (
-     
-        <section className="catalog">       
+        <section className="catalog">
+            
             <div className="container">
-           
-            <div className="welcome-container hidden">
-                <div className="catalog-text">
-                    <h1 className="welcome-msg hidden"><span>Your</span> home should tell the story of who you are,
-                        <br className='hidden'></br>and be a collection of what you love.</h1>
-                </div>              
-            </div>
-         
-
-           
-            <div className="introduction-container hidden">
-                <p className='hidden'>Where we bring you the finest collection of furniture pieces that will transform your home into a sophisticated haven of style and comfort.</p>
-                <p className='hidden'>Our exclusive collection features the finest handcrafted furniture made from the highest quality materials and designed by world-renowned artisans.</p>
-                <p className='hidden'> Each piece is a masterpiece that exudes elegance, luxury, and sophistication.</p>
-                <p className='hidden'></p>
-                <NavLink className="explore-btn hidden" to="/products">Explore our collections</NavLink>
-            </div>
-           
- 
-        
-            <div className="trending-container hidden">
-                <h3 className='hidden'>Trending products:</h3>
-                <div className='trendingProducts-container hidden'>
-                    {trendingProducts.map(x => <TrendingProductCard key={x._id} productInfo = {x} />)}
+                <div className="welcome-container hidden">
+                    <div className="catalog-text">
+                        <h1 className="welcome-msg hidden"><span>Your</span> home should tell the story of who you are,
+                            <br className='hidden'></br>and be a collection of what you love.</h1>
+                    </div>
                 </div>
-            </div>
-           
 
-          
-            <div className="resell-container hidden">
-                <p className='hidden'>We fully support the idea of selling old furniture to help save the environment.</p>
-                <p className='hidden'>By selling your old furniture from our website we are donating 5% of the price of the product to organizations that are helping the enviroment. 
-                We believe that every small step counts towards a better future, and by selling your old furniture, you're making a significant contribution to protecting the planet.
-               So, if you're looking for a sustainable and eco-friendly way to furnish your home, we're here to help.</p>
-                <p className='hidden'>Thank you for supporting our mission to create a greener and more sustainable future.</p>
-                <p className='hidden'></p>
-                <NavLink className="sell-btn hidden" to="/recycle">Retail your old furniture</NavLink>
+
+
+                <div className="introduction-container hidden">
+                    <p className='hidden'>Where we bring you the finest collection of furniture pieces that will transform your home into a sophisticated haven of style and comfort.</p>
+                    <p className='hidden'>Our exclusive collection features the finest handcrafted furniture made from the highest quality materials and designed by world-renowned artisans.</p>
+                    <p className='hidden'> Each piece is a masterpiece that exudes elegance, luxury, and sophistication.</p>
+                    <p className='hidden'></p>
+                    <NavLink className="explore-btn hidden" to="/products">Explore our collections</NavLink>
+                </div>
+
+
+
+                <div className="trending-container hidden">
+                    <h3 className='hidden'>Trending products:</h3>
+                    <div className='trendingProducts-container hidden'>
+                        {trendingProducts.map(x => <TrendingProductCard key={x._id} productInfo={x} />)}
+                    </div>
+                </div>
+
+
+
+                <div className="resell-container hidden">
+                    <p className='hidden'>We fully support the idea of selling old furniture to help save the environment.</p>
+                    <p className='hidden'>By selling your old furniture from our website we are donating 5% of the price of the product to organizations that are helping the enviroment.
+                        We believe that every small step counts towards a better future, and by selling your old furniture, you're making a significant contribution to protecting the planet.
+                        So, if you're looking for a sustainable and eco-friendly way to furnish your home, we're here to help.</p>
+                    <p className='hidden'>Thank you for supporting our mission to create a greener and more sustainable future.</p>
+                    <p className='hidden'></p>
+                    <NavLink className="sell-btn hidden" to="/recycle">Retail your old furniture</NavLink>
+                </div>
+
             </div>
-          
-            </div>
-           
+
         </section>
-       
+
     )
 }
 
