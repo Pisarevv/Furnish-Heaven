68a2c3f8626a24cb5de8b948289b5fa94b35be30
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-04-01 19:03:19 +0300

    Created reducer for user product create and edit forms

---
+++ client/src/reducers/userProductReducer.js
@@ -0,0 +1,36 @@
+function userProductReducer(state, action) {
+    switch (action.type) {
+        case "SET_CATEGORY":
+            return { ...state, category: action.payload, categoryError: "" };
+        case "SET_DESCRIPTION":
+            return { ...state, description: action.payload, descriptionError: "" };
+        case "SET_IMG_URL":
+            return { ...state, imgUrl: action.payload, imgUrlError: "" };
+        case "SET_MODEL":
+            return { ...state, model: action.payload, modelError: "" };
+        case "SET_TYPE":
+            return { ...state, type: action.payload, typeError: "" };
+        case "SET_PRICE":
+            return { ...state, price: action.payload, priceError: "" };
+        case "SET_YEAR":
+            return { ...state, year: action.payload, yearError: "" };
+        case "SET_CATEGORY_ERROR":
+            return { ...state, categoryError: action.payload };
+        case "SET_DESCRIPTION_ERROR":
+            return { ...state, descriptionError: action.payload };
+        case "SET_IMG_URL_ERROR":
+            return { ...state, imgUrlError: action.payload };
+        case "SET_MODEL_ERROR":
+            return { ...state, modelError: action.payload };
+        case "SET_TYPE_ERROR":
+            return { ...state, typeError: action.payload };
+        case "SET_PRICE_ERROR":
+            return { ...state, priceError: action.payload };
+        case "SET_YEAR_ERROR":
+            return { ...state, yearError: action.payload };
+        default:
+            return state;
+    }
+}
+
+export default userProductReducer;
