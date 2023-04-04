4772cfa2f68a385f2dca8adb259580b5eb44906d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-04-04 14:35:08 +0300

    Added documentation

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,3 +1,23 @@
+/**
+ * useLocalStorage 
+ * ---------------------
+ * Custom hook that allows data to be stored
+ * in the browser localStorage and be modified easily.
+ * On initializing it recieves a default value that is 
+ * associated with the given key.
+ * ---------------------- 
+ * 
+ * States:
+ * ----------------------
+ * - value (object): This state allows any type of data to be stored.
+ * ---------------------
+ * 
+ * Functions: 
+ * - setLocalStorageValue - function that changes the stored value 
+ *  in the browser localStorage and update the state of the hook
+**/
+
+
 import { useState } from "react";
 
 const useLocalStorage = (key, defaultValue) => {
