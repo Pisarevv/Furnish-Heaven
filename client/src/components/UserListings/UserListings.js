b1020134f68177aff77f4781370a81c3126a7064
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-04-06 16:20:53 +0300

    Added documentation

--- client/src/components/UserListings/UserListings.js
+++ client/src/components/UserListings/UserListings.js
@@ -1,11 +1,50 @@
+/**
+ * UserListings  Component
+ * ---------------------
+ * This component displays the products that the user has created
+ * listings for. He can edit or delete listing.
+ * ---------------------- 
+ * 
+ * States:
+ * ----------------------
+ * - userListings (array): The collection holding the fetched user listings products products from the server.
+ * ---------------
+ * 
+ *  * Contexts:
+ * ----------------
+ * - useAuthContext
+ *  In this component this context provides the "user" object.
+ *  The purpose of this object here is to identify if the user,
+ *  his and his listings.
+ *  ---------------
+ * 
+ * Functions:
+ * -----------------
+ * - Observe 
+ *  This function is used for the animation of fading in in the page.
+ * 
+ * - ErrorHandler
+ *  This is a custom function that handles errors thrown by the REST api  
+ *  and based on the error shows the user notifications.
+ *  
+ *  - setLoading 
+ *  This function removes the loading animation.
+ * -----------------
+**/
+
+
 import { useEffect, useState } from "react";
 import { useAuthContext } from "../../contexts/AuthContext";
-import IsLoadingHOC from "../Common/IsLoadingHoc";
-import { getAllUserListings } from "../../services/userProductsService";
-import Observe from "../../utils/Observer";
-import { ErrorHandler } from "../../utils/ErrorHandler/ErrorHandler";
-import UserProductCard from "../Recycle/UserProductCard";
 import { NavLink } from "react-router-dom";
+
+import { ErrorHandler } from "../../utils/ErrorHandler/ErrorHandler";
+
+import { getAllUserListings } from "../../services/userProductsService";
+
+import IsLoadingHOC from "../Common/IsLoadingHoc";
+import Observe from "../../utils/Observer";
+
+import UserProductCard from "../Recycle/UserProductCard";
 
 import './UserListings.css'
 
