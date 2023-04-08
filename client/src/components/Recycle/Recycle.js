4a8f85007ba7c596fee6244009184282c064c3a0
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-04-08 18:20:27 +0300

    Minor changes

--- client/src/components/Recycle/Recycle.js
+++ client/src/components/Recycle/Recycle.js
@@ -71,7 +71,7 @@ const Recycle = (props) => {
     const { setLoading } = props;
 
     //Pagination
-    const [itemsPerPage, setItemsPerPage] = useState(9);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
     const [userProductsCount, setUserProductsCount] = useState(0);
     const { page: currentPage } = useParams();
 
